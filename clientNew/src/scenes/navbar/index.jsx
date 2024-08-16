f5d770f6c832edebc6f10b7cced54962f70a4405
rismanobahar <rismanobahar@gmail.com>
2024-08-16 17:00:52 +0700

    8/16/2024

--- clientNew/src/scenes/navbar/index.jsx
+++ clientNew/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          SocioMedia
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
--- clientNew/src/scenes/widgets/FriendListWidget.jsx
+++ clientNew/src/scenes/widgets/FriendListWidget.jsx
@@ -9,7 +9,7 @@ const FriendListWidget = ({ userId }) => {
   const dispatch = useDispatch();
   const { palette } = useTheme();
   const token = useSelector((state) => state.token);
-  const friends = useSelector((state) => state.user.friends);
+  const friends = useSelector((state) => state.user.friends); 
 
   const getFriends = async () => {
     const response = await fetch(
